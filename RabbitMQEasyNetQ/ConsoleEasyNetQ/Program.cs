84fa5ecc2bbd315b16e494fbd5b3f6a3c798438c
Ivan Meireles da Silva <isilva@meuacesso.com.br>
2019-10-10 15:02:56 -0300

    teste publicar msg na exchange

---
+++ RabbitMQEasyNetQ/ConsoleEasyNetQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using EasyNetQ;
+using EasyNetQ.Topology;
+
+namespace ConsoleEasyNetQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var advancedBus = RabbitHutch.CreateBus("host=localhost").Advanced;
+
+            var sourceExchange = advancedBus.ExchangeDeclare("test_exchange", ExchangeType.Topic);
+            var queue = advancedBus.QueueDeclare("test_queue");
+
+            advancedBus.Bind(sourceExchange, queue, "test.exchange");
+        }
+    }
+}
---
+++ RabbitMQEasyNetQ/RabbitMQEasyNetQ/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.Topology;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQEasyNetQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet("{message}")]
+        public async Task<IActionResult> Add(string message)
+        {
+            using var bus = RabbitHutch.CreateBus("host=rabbit").Advanced;
+            var exchange = bus.ExchangeDeclare("test_exchange", ExchangeType.Topic, durable: true);
+            bus.Publish(exchange, "test.exchange", false, new MessageProperties { CorrelationId = Guid.NewGuid().ToString() }, Encoding.ASCII.GetBytes(message));
+
+            return Ok();
+        }
+    }
+}
